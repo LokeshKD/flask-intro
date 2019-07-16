6236da32a59f312d2a2cfd6a114d89dcd1a32efc
Lokesh <i.lokesh@gmail.com>
2019-07-16 21:57:55 +0530

    Ability to add Posts per user

--- project/home/views.py
+++ project/home/views.py
@@ -3,8 +3,9 @@
 ###
 from project import app, db
 from project.models import BlogPost
-from flask import render_template, Blueprint
-from flask_login import login_required
+from project.home.forms import MessageForm
+from flask import render_template, Blueprint, flash, url_for, redirect, request
+from flask_login import login_required, current_user
 
 ###
 # Config
@@ -31,11 +32,26 @@ def login_required(f):
 # Routes
 ###
 
-@home_blueprint.route('/')
-@login_required
+# use decorators to link the function to a url
+@home_blueprint.route('/', methods=['GET', 'POST'])   # pragma: no cover
+@login_required   # pragma: no cover
 def home():
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template(
+            'index.html', posts=posts, form=form, error=error)
 
 @home_blueprint.route('/welcome')
 def welcome():
